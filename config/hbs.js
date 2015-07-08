7f4a1287270c6212b7fa642c2d5a56e94e553aa7
Artur Tsurkan <arturtsurkan@gmail.com>
2015-07-08 06:04:09 -0400

    Added resumes folder to .gitignore. We tried to use forms/jquery but ended up ditching that for now. Completed validation on application form. If it fails, we return whatever the user inputted back to the form. This involved adding an HBS helper to prefill form selects.

--- config/hbs.js
+++ config/hbs.js
@@ -19,4 +19,14 @@ module.exports = function() {
     blocks[name] = [];
     return val;
   });
-}
+
+  hbs.registerHelper('select', function(value, options) {
+    return options.fn(this)
+      .split('\n')
+      .map(function(v) {
+        var t = 'value="' + value + '"'
+        return ! RegExp(t).test(v) ? v : v.replace(t, t + ' selected="selected"')
+      })
+      .join('\n');
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,49 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    multer = require('multer'),
+    Application = require('../models/application.js');
 
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/apply', function(req, res, next) {
-  res.render('applications/create');
-});
+router.route('/apply')
+  .get(function(req, res, next) {
+    res.render('applications/create', {
+      app: new Application(),
+    });
+  })
+  .post([
+    multer({
+      dest: './public/resumes',
+      rename: function (fieldname, filename) {
+        return filename.replace(/\W+/g, '-').toLowerCase() + Date.now();
+      },
+      onFileUploadStart: function (file, req, res) {
+        if (file.extension != 'pdf') {
+          req.body.fileType = 'INVALID'
+          return false;
+        }
+      }
+    }),
+    function(req, res) {
+      if (req.body.fileType == 'INVALID') {
+        req.flash('messages', {
+          style: 'danger', 
+          type: 'Error',
+          text: 'You must upload your resume in PDF format.',
+        });
+        res.locals.messages = req.flash('messages');
+        console.log(req.body);
+        res.render('applications/create', {
+          app: req.body,
+        });
+      } else {
+        console.log(req.body);
+        console.log(req.files);
+        res.redirect('/apply');
+      }
+    }
+  ]);
 
 module.exports = router;
